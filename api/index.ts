67432c1d1d5eb6729a4e84c4b011be17fe8bb344
submarine <frada6688q@gmail.com>
2024-11-16 23:29:19 +0700

    feat: fresh

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,18 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { startVercel } from '../src';
+import { bot } from '../src';
+import { webhookCallback } from 'grammy';
+
+function runMiddleware(req: VercelRequest, res: VercelResponse, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handle(req: VercelRequest, res: VercelResponse) {
-  try {
-    await startVercel(req, res);
-  } catch (e: any) {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
-    console.error(e.message);
-  }
+  await runMiddleware(req, res, webhookCallback(bot, 'https'));
 }
--- src/core/production.ts
+++ src/core/production.ts
@@ -1,28 +1,12 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
 import createDebug from 'debug';
-import { Bot, webhookCallback } from 'grammy';
+import { Bot } from 'grammy';
 
 const debug = createDebug('bot:dev');
 
 const PORT = (process.env.PORT && parseInt(process.env.PORT, 10)) || 3000;
 const VERCEL_URL = `${process.env.VERCEL_URL}`;
 
-function runMiddleware(req: VercelRequest, res: VercelResponse, fn: any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
-}
-
-const production = async (
-  req: VercelRequest,
-  res: VercelResponse,
-  bot: Bot,
-) => {
+const production = async (bot: Bot) => {
   debug('Bot runs in production mode');
   debug(`setting webhook: ${VERCEL_URL}`);
   if (!VERCEL_URL) {
@@ -35,12 +19,5 @@ const production = async (
     debug(`setting webhook: ${VERCEL_URL}/api`);
     await bot.api.setWebhook(`${VERCEL_URL}/api`);
   }
-
-  if (req.method === 'POST') {
-    await runMiddleware(req, res, webhookCallback(bot, 'https'));
-  } else {
-    res.status(200).json('Listening to bot events...');
-  }
-  debug(`starting webhook on port: ${PORT}`);
 };
 export { production };
